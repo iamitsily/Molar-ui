7db1cdb005489e1ad66f8ec49e1e73b5f86ef28d
iamitsily <ilystx1@gmail.com>
2023-02-28 12:26:07 -0600

    [yahir]enproceso

--- app/src/main/java/com/haku/molar/ui/menuPacienteLayout.java
+++ app/src/main/java/com/haku/molar/ui/menuPacienteLayout.java
@@ -2,19 +2,16 @@ package com.haku.molar.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.haku.molar.R;
 
-public class MainActivity extends AppCompatActivity {
+
+public class menuPacienteLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this,menuPacienteLayout.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_menu_paciente_layout);
     }
 }
