b14f98c66d3850f055edc0b59951fc9bffdff873
ArielYosefMR <arielyosefmontiel@gmail.com>
2023-03-14 09:45:27 -0600

    [Ariel]Interfaz_AjustesDeCuentaMenuLayout

---
+++ app/src/main/java/com/haku/molar/ui/AjustesDeCuentaMenuLayout.java
@@ -0,0 +1,14 @@
+package com.haku.molar.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjustesDeCuentaMenuLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajustes_de_cuenta_menu_layout);
+    }
+}
