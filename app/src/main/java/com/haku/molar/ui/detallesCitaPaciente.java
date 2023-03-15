e92461ab9ee75d26a4f4614d261ea5c5d48c994e
Sergioeling <20207487@uatx.mx>
2023-03-14 22:58:04 -0600

    [Sergio]DetallesDeCitaPacienteLayout

---
+++ app/src/main/java/com/haku/molar/ui/detallesCitaPaciente.java
@@ -0,0 +1,15 @@
+package com.haku.molar.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.haku.molar.R;
+
+public class detallesCitaPaciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_cita_paciente);
+    }
+}
+
