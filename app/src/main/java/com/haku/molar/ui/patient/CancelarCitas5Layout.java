1b83b93c20f8b43577b652b4ba9804e5583335c2
ArielYosefMR <arielyosefmontiel@gmail.com>
2023-03-22 16:36:03 -0600

    [Ariel] Interfaz de CancelarCitas5, falta el layout para el formato de los items del listview

---
+++ app/src/main/java/com/haku/molar/ui/patient/CancelarCitas5Layout.java
@@ -0,0 +1,14 @@
+package com.haku.molar.ui.patient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CancelarCitas5Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancelar_citas5_layout);
+    }
+}
