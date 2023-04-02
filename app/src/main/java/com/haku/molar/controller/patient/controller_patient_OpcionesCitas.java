56a2a91bccd9da0afd822a34a17f2022594ef816
Sergioeling <20207487@uatx.mx>
2023-04-02 00:25:12 -0600

    [tiki]ModificacionesIMG

---
+++ app/src/main/java/com/haku/molar/controller/patient/controller_patient_OpcionesCitas.java
@@ -0,0 +1,14 @@
+package com.haku.molar.controller.patient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.haku.molar.R;
+
+public class controller_patient_OpcionesCitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_patient_opcionescitas);
+    }
+}
---
+++ app/src/main/java/com/haku/molar/controller/patient/controller_patient_ReagendarCitas.java
@@ -0,0 +1,18 @@
+package com.haku.molar.controller.patient;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.haku.molar.R;
+
+public class controller_patient_ReagendarCitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_patient_reagendar_citas);
+    }
+}
+
+
