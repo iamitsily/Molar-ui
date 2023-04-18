ef4712c3374b7e03b1a0f17764133682ae41a645
iamitsily <ilystx1@gmail.com>
2023-04-18 00:32:29 -0600

    [yahir]fixrepo

---
+++ app/src/main/java/com/haku/molar/model/patient/model_Patient.java
@@ -0,0 +1,101 @@
+package com.haku.molar.model.patient;
+
+public class model_Patient {
+    int id, rol, sexo;
+    String nombre, apellidoPaterno, apellidoMaterno, email, telefono, password;
+
+    //Constructores
+        //Constructor general
+    public model_Patient(int id, int rol, int sexo, String nombre, String apellidoPaterno, String apellidoMaterno, String email, String telefono, String password) {
+        this.id = id;
+        this.rol = rol;
+        this.sexo = sexo;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.email = email;
+        this.telefono = telefono;
+        this.password = password;
+    }
+
+
+    //Funciones
+        //Ejemplo - Funcion al back para registrarPaciente
+    public int registrarPaciente(model_Patient paciente){
+
+        return 0;
+    }
+
+    //Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
