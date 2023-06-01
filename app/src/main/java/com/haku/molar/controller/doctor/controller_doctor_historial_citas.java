90eb153c92a1db1cc66544797f19cd633c6237fb
iamitsily <20206243@uatx.mx>
2023-06-01 15:06:36 -0600

    [yahir]UIAdmin

--- app/src/main/java/com/haku/molar/controller/doctor/controller_doctor_historial_citas.java
+++ app/src/main/java/com/haku/molar/controller/doctor/controller_doctor_historial_citas.java
@@ -7,9 +7,6 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.haku.molar.R;
-import com.haku.molar.controller.patient.controller_patient_MenuPaciente;
-import com.haku.molar.controller.patient.controller_patient_NotificacionesPaciente;
-import com.haku.molar.controller.patient.controller_patient_OpcionesCitas;
 
 public class controller_doctor_historial_citas extends AppCompatActivity {
     BottomNavigationView menuNav;
@@ -20,22 +17,22 @@ public class controller_doctor_historial_citas extends AppCompatActivity {
         setContentView(R.layout.view_doctor_historial_citas);
 
         menuNav = findViewById(R.id.menu_patient_menu);
-        menuNav.setSelectedItemId(R.id.menu_patient_historial);
+        menuNav.setSelectedItemId(R.id.menu_doctor_historial);
 
         //Listeners
         menuNav.setOnItemSelectedListener(item ->{
             switch (item.getItemId()){
-                case R.id.menu_patient_home:
+                case R.id.menu_doctor_home:
                     startActivity(new Intent(this, controller_doctor_menu_doctor.class));
                     overridePendingTransition(R.anim.menu_patient_slide_in_right, R.anim.menu_patient_slide_out_left);
                     finish();
                     break;
-                case R.id.menu_patient_cita:
-                    startActivity(new Intent(this, controller_doctor_citas.class));
+                case R.id.menu_doctor_pacientes:
+                    startActivity(new Intent(this, controller_doctor_listaPacientes.class));
                     overridePendingTransition(R.anim.menu_patient_slide_in_right, R.anim.menu_patient_slide_out_left);
                     finish();
                     break;
-                case R.id.menu_patient_notificacion:
+                case R.id.menu_doctor_notificaciones:
                     startActivity(new Intent(this, controller_doctor_notificaciones.class));
                     overridePendingTransition(R.anim.menu_patient_slide_in_right, R.anim.menu_patient_slide_out_left);
                     finish();
