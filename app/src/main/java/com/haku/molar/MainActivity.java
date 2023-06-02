7ff460bb6c603b533a989a7caf4893e68b1e2781
iamitsily <ilystx1@gmail.com>
2023-06-01 19:37:24 -0600

    [yahir]fixmail

--- app/src/main/java/com/haku/molar/MainActivity.java
+++ app/src/main/java/com/haku/molar/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.haku.molar.controller.patient.*;
 import com.haku.molar.general.controller_General_Login;
+import com.haku.molar.utils.Network;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/haku/molar/utils/MolarMail.java
+++ app/src/main/java/com/haku/molar/utils/MolarMail.java
@@ -1,8 +1,19 @@
 package com.haku.molar.utils;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -18,10 +29,11 @@ import javax.mail.internet.MimeMessage;
 public class MolarMail {
     String Remitente, RemitentePass="webysycthvywcvlf" ,Receptor, PassReceptor, MatriculaReceptor, NombreReceptor, codigoRecuperacion;
     Context context;
+    MolarConfig molarConfig = new MolarConfig();
 
     public MolarMail() {
     }
-
+    //controller_assistant_registrarPaciente
     public MolarMail(String receptor, String passReceptor, String matriculaReceptor, String nombreReceptor, Context context) {
         Receptor = receptor;
         PassReceptor = passReceptor;
@@ -29,13 +41,14 @@ public class MolarMail {
         NombreReceptor = nombreReceptor;
         this.context = context;
     }
-
+    //controller_general_login -> recuperarPass
     public MolarMail(String receptor, String codigoRecuperacion, Context context) {
         Receptor = receptor;
         this.codigoRecuperacion = codigoRecuperacion;
         this.context = context;
     }
-    public void sendMail() {
+    public void sendMailRegistro() {
+        System.out.println("sendMailRegistro");
         Remitente = "molar.haku@gmail.com";
 
         String stringHost = "smtp.gmail.com";
@@ -85,8 +98,39 @@ public class MolarMail {
             throw new RuntimeException(e);
         }
     }
-
-    public void codeMail() {
+    public void sendMailRegistroRedMovil(){
+        System.out.println("sendMailRegistroRedMovil");
+        String url = molarConfig.getDomainAzure()+"/general/email.php";
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equalsIgnoreCase("Enviado correctamente")) {
+                    System.out.println("sendMailRegistro exitoso");
+                }else {
+                    System.out.println("sendMailRegistro fallido");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "Error: "+error.getMessage(), Toast.LENGTH_SHORT).show();
+                System.out.println("Error: "+error.getMessage());
+            }
+        }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("destino",Receptor);
+                params.put("asunto","Detalles de Cuenta - Molar");
+                params.put("mensaje","Haku Molar<br><br>Hola " + NombreReceptor + " te damos la biendenida a Molar, <br>para nosotros es un placer que formes parte de nuestra familia.<br><br>A continuación tus datos de acceso: <br><br>Matricula: "+ MatriculaReceptor + "<br>Contraseña: "+ PassReceptor + "<br><br>Te recomendamos cambiar tu Contraseña en Perfil -> Ajustes de cuenta -> Configuración de perfil<br><br>Gracias por confiar en osotros.<br>Con amor, Molar <3<br><br>Email generado automaticamente, no responder.");
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(request);
+    }
+    public void codeMailFP() {
         Remitente = "molar.haku@gmail.com";
 
         String stringHost = "smtp.gmail.com";
@@ -135,7 +179,38 @@ public class MolarMail {
             throw new RuntimeException(e);
         }
     }
-
+    public void codeMailFPRedMovil(){
+        System.out.println("sendMailRegistroRedMovil");
+        String url = molarConfig.getDomainAzure()+"/general/email.php";
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equalsIgnoreCase("Enviado correctamente")) {
+                    System.out.println("sendMailRegistro exitoso");
+                }else {
+                    System.out.println("sendMailRegistro fallido");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "Error: "+error.getMessage(), Toast.LENGTH_SHORT).show();
+                System.out.println("Error: "+error.getMessage());
+            }
+        }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("destino",Receptor);
+                params.put("asunto","Código de autentificación - Molar");
+                params.put("mensaje","Haku Molar<br><br>Hola! Recibimos una solicitud para restablecer su contraseña de Molar,<br>Si no ha solicitado un restablecimiento de contraseña, puede ignorar este correo electronico.<br><br>Codigo de recuperación: <br><br>Codigo: "+ codigoRecuperacion +"<br><br>Ingrese el codigo en la aplicación, este codigo expira en cuanto cierrre la app o pantalla de recuperación<br><br>Con amor, Molar <3<br><br>Email generado automaticamente, no responder.");
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(request);
+    }
     public String getRemitente() {
         return Remitente;
     }
