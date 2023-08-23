ed6a46b8e8cfbd3ece5e8bfe1132dd0e7fd1605c
iamitsily <ilystx1@gmail.com>
2023-08-23 13:08:24 -0600

    [Yahir]fix-domainAzure

--- app/src/main/java/com/haku/molar/utils/MolarConfig.java
+++ app/src/main/java/com/haku/molar/utils/MolarConfig.java
@@ -4,8 +4,8 @@ public class MolarConfig {
     //ServiceMolarNormal -> https://molarservices.azurewebsites.net
     //ServiceMolarAux -> https://molarservicesaux.azurewebsites.net (Normalmente apagado, solo para emergencias)
     //Local -> http://[DireccionIpPropia]/Molar-Backend
-    private String domainAzure = "https://molarservices.azurewebsites.net";
-
+    //private String domainAzure = "https://molarservices.azurewebsites.net";
+    private String domainAzure = "https://molarinsano.azurewebsites.net";
      public String getDomainAzure() {
           return domainAzure;
      }
